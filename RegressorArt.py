304511e7ea001b3092d5c1bcc0083a3aa1d80c97
wa721 <willjashford@gmail.com>
2017-07-05 20:32:55 +0100

    adds a functional single sample training script

--- RegressorArt.py
+++ RegressorArt.py
@@ -6,32 +6,82 @@ from sklearn.neural_network import MLPRegressor #the regressor
 import pandas as pd #data wrangling
 import matplotlib as plot #for plotting of the ANN structure or "art"
 import cv2 #for presenting the "art"
+import numpy as np #for some image transformations
+import urllib #to scrape the images from URLs
 
 #import some training da
-ExampleArt = []
-maxNumImages
-for image in range(1,maxNumImages):
-    ExampleArt[image] = pd.read_csv(####)
+URLs = pd.read_csv("painting_dataset.csv")
 
-#to be determined
+def url_to_image(url):
+	# download the image, convert it to a NumPy array, and then read
+	# it into OpenCV format
+    resp = urllib.urlopen(url)
+    image = np.asarray(bytearray(resp.read()), dtype="uint8")
+    image = cv2.imdecode(image, cv2.IMREAD_COLOR)
+    #print image
+	# return the image
+    #cv2. imwrite( "image.jpg", image)
+    return image
+
+#create a container for the image arrays
+ExampleArt = []
+labels = []
+#specify the number of images (n-1)
+maxNumImages = 0
+
+#scrape the images
+for image in range(0,1):
+    IIQ = url_to_image(URLs.iloc[image,0]) #image in question
+    labels.append(URLs.iloc[image,3])
+    artItem = []
+    for pixel in range(0,len(IIQ)-1):
+        for section in range(0,540):
+            artItem.append(IIQ[pixel][section][0])
+            artItem.append(IIQ[pixel][section][1])
+            artItem.append(IIQ[pixel][section][2])
+    ExampleArt.append(artItem)
+    print len(IIQ[0])
+    print len(IIQ)
+
+
+for item in range(0,len(labels)):
+    if labels[item] == "cow":
+        labels[item] = 1
+    else:
+        labels[item] = 2
 
 #build the regressor
 
 #this is the basic structure from sklearn including all of the parameters as a placeholder
-artist = MLPRegressor(hidden_layer_sizes=(3,5,10), activation='relu', solver='adam', alpha=0.0001, batch_size='auto', learning_rate='constant', learning_rate_init=0.001, power_t=0.5, max_iter=200, shuffle=True, random_state=None, tol=0.0001, verbose=False, warm_start=False, momentum=0.9, nesterovs_momentum=True, early_stopping=False, validation_fraction=0.1, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
-
+artist = MLPRegressor(hidden_layer_sizes=(3,5,10), activation='relu', solver='adam', alpha=0.0001, batch_size='auto', learning_rate="constant")
 
 #train the artist!
-artist.fit(TrainingParameters,ExampleArt)
+#example art is the y variable, labels is the x (remember we are telling the machine what to paint!)
+
+artist.fit(labels,ExampleArt)
 
 #specify the parameters of the "art" to be produced.
 # list as [reds,greens,blues,subject,#objects,mood]
-inspiration = [20,50,20,2,50,10]
+inspiration = [2]
 
 #make the art
 artwork = artist.predict(inspiration)
+artwork = artwork[0]
+m = max(artwork)
+for colour in range(0,(len(artwork)-1)):
+    artwork[colour] = int(artwork[colour]/m*255)
+print artwork
+#save the artwork!
 
-#plot the artwork!
-cv.imshow('dst_rt', artwork)
-cv.waitKey(0)
-cv.destroyAllWindows()
+ImageContainer = []
+pixelCount = 0
+for pixel in range(0,len(IIQ)-1):
+    ImageContainer.append([])
+    for section in range(0,540):
+        ImageContainer[pixel].append(np.array([artwork[pixelCount],artwork[pixelCount+1],artwork[pixelCount+2]]))
+        pixelCount += 3
+    ImageContainer[pixel] = np.asarray(ImageContainer[pixel])
+
+ImageContainer = np.asarray(ImageContainer)
+
+cv2.imwrite("masterpiece.jpg",ImageContainer)
